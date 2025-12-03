e3527ccbe8ca031c2a9a2e1ee7bad78b92c4bd31
ny0o0om <nora.pray8@gmail.com>
2025-12-03 15:27:49 -0600

    Finished updateEmployee.js, as well as updated the index.js to fixed a bug

--- staffCLI/commands/admin/updateEmployee.js
+++ staffCLI/commands/admin/updateEmployee.js
@@ -1,6 +1,6 @@
-import { supabaseServiceRoleClient } from "../../library/supabase-client.js";
+import { getSupabaseServiceRoleClient } from "../../library/supabase-client.js";
 
-const SUPORTED_FIELDS = [
+const SUPPORTED_FIELDS = [
   "name",
   "mobile",
   "email",
@@ -12,6 +12,7 @@ const SUPORTED_FIELDS = [
 ];
 
 const updateEmployeeAction = async (session, employeeID, field, value) => {
+  const supabaseClient = getSupabaseServiceRoleClient();
   if (!employeeID || !field || !value) {
     console.log("Usage: update-employee <employee_id> <field> <new_value>");
     console.log(`Supported fields: ${SUPPORTED_FIELDS.join(", ")}`);
@@ -33,18 +34,46 @@ const updateEmployeeAction = async (session, employeeID, field, value) => {
     return;
   }
 
-  let finalValue = value;
+  let finalValue = null;
 
   switch (lowerField) {
-    case "name":
-      break;
     case "mobile":
+      const mobile = value;
+      if (mobile.length() !== 9 || isNaN(mobile)) {
+        console.error(`Error: Invalid Phone number.`);
+        return;
+      }
+      finalValue = mobile;
       break;
     case "email":
+      const email = value;
+      const regexEmail = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+      if (!regexEmail.test(email)) {
+        console.error(`Error: Invalid Email.`);
+        return;
+      }
+      finalValue = email;
+      break;
+    case "name":
+    case "address":
+      const stringValue = value.toString();
+      console.log(value);
+      if (stringValue.length < 0) {
+        console.error(`Error: Invalid Password`);
+        return;
+      }
+      let newString = stringValue.replace(/_/g," ");
+      console.log(newString);
+      finalValue = newString;
       break;
     case "password":
-      break;
-    case "address":
+      const stringPassword = value;
+      console.stringPassword(value);
+      if (stringValue.toString().length < 0) {
+        console.error(`Error: Invalid Password`);
+        return;
+      }
+      finalValue = stringPassword;
       break;
     case "salary":
       const numericSalary = parseInt(value);
@@ -56,25 +85,54 @@ const updateEmployeeAction = async (session, employeeID, field, value) => {
       break;
     case "role":
       const validRoles = ["owner", "employee"];
-      if (!validRoles.includes(finalValue)) {
+      if (!validRoles.includes(value)) {
         console.error(
           `Error: Invalid role '${value}'. Must be one of: ${validRoles.join(
             ", "
           )}.`
         );
-        return;
+        break;
       }
       break;
     case "routingnumber":
-      numericRoutingNumber = value;
-      if (isNaN(numericRoutingNumber) || length)
+      const stringRoutingNumber = parseInt(value);
+      if (
+        isNaN(stringRoutingNumber) ||
+        stringRoutingNumber.toString().length !== 9
+      ) {
+        console.log(stringRoutingNumber.toString().length);
+        console.error("Error: Routing Number must be 9 digits long.");
+        return;
+      }
+      finalValue = stringRoutingNumber;
       break;
     default:
+      console.error("Error: Invalid Value for field");
       break;
   }
-};
 
-console.log(finalValue);
+  updateData[field] = finalValue;
+
+  console.log(
+    `[${session.role.toUpperCase()}] Updating Employee ID ${id} set ${field} to ${finalValue}...`
+  );
+
+  const { data, error } = await supabaseClient
+    .from("employee")
+    .update(updateData)
+    .eq("employeeid", id)
+    .select(`employeeid, ${lowerField}`);
+
+  if (error) {
+    console.error(`ERROR updating Employee: ${error.message}`);
+  } else if (data.length === 0) {
+    console.log(`Employee ID ${id} not found.`);
+  } else {
+    console.log(
+      `Employee ID ${id} updated successfully! New ${field}: ${data[0][lowerField]}`
+    );
+  }
+};
 
 export default {
   name: "update-employee",
